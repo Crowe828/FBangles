2ed95052231e4249fe3e47cb11baf6a41ce2cf13
crowe828 <69489498+crowe828@users.noreply.github.com>
2021-05-13 17:35:13 -0400

    added styling to navbar and added images to middle section of homepage

--- fbangles/src/app/components/home/home.component.ts
+++ fbangles/src/app/components/home/home.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  images: any[] = [
+    'https://blogs.3ds.com/northamerica/wp-content/uploads/sites/4/2019/08/Robots-Square.jpg',
+    'https://scx2.b-cdn.net/gfx/news/hires/2019/3-robot.jpg',
+    'https://spectrum.ieee.org/image/MzY4MjM4MA.jpeg',
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
